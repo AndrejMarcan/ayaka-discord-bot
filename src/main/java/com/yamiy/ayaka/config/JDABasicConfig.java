a5e769a9bddf668f045c1aafcd03bc061dab8fbe
Andrej Marčan <52137644+YatemYami@users.noreply.github.com>
2020-11-16 13:00:16 +0100

    Update JDABasicConfig.java

--- src/main/java/com/yamiy/ayaka/config/JDABasicConfig.java
+++ src/main/java/com/yamiy/ayaka/config/JDABasicConfig.java
@@ -8,6 +8,7 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class JDABasicConfig {
+	/** token here is invalid, you have to get your own drom https://discord.com/developers/applications */
 	private static final String token = "Nzc0OTIxNjE0MjAxNTg1NzE0.X6ez_g.kbQKqiGj4YtgorHvVmgPX659I8A";
 	
 	public static JDABuilder buildJDA() throws LoginException {
